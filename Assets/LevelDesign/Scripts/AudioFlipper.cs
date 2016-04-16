89b962f6ccbf278dccbea70b361426fa7ca07914
zoox101 <william@thebookers.net>
2016-04-16 03:00:05 -0500

    Working Music Test
    Press space bar on the test scene.

---
+++ Assets/LevelDesign/Scripts/AudioFlipper.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioFlipper : MonoBehaviour {
+
+	public static int currentaudioplayer = 0;
+	public static int totalaudioplayers = 0;
+
+	int audioplayerid;
+	AudioSource audiosource;
+
+	// Use this for initialization
+	void Start () {
+		audiosource = GetComponent<AudioSource> ();
+		audiosource.mute = true;
+		audioplayerid = totalaudioplayers;
+		totalaudioplayers++;
+		flip ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (audioplayerid == currentaudioplayer)
+			audiosource.mute = false;
+		else
+			audiosource.mute = true;
+	}
+
+	public static void flip() {
+		currentaudioplayer = (currentaudioplayer + 1) % totalaudioplayers;
+	}
+}
