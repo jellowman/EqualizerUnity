f99d9daccfcc9c201f7244a91122ed7a5aaff6a9
Eric Garrison <egarrisn@swbell.net>
2016-04-16 18:49:05 -0500

    Initial work on Enemy
    Created enemy folder/script.  Began work on selecting targets

---
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : BaseCharacter {
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void FixedUpdate() {
+		BaseCharacter[] targets = GameObject.FindObjectsOfType<BaseCharacter> ();
+		GameObject[] obstacles = GameObject.FindGameObjectsWithTag("Wall");
+		double max = double.MaxValue;
+		BaseCharacter target = null;
+		foreach (BaseCharacter bc in targets) {
+			if (!(this.CompareTag (bc.tag))) {
+				double dist = Mathf.Sqrt ((bc.transform.position.x - this.transform.position.x) + (bc.transform.position.y - this.transform.position.y));
+				if (dist < max) {
+					max = dist;
+					null = bc;
+				}
+			}
+		}
+
+		movex = Input.GetAxis (horizontalAxis);
+		movey = Input.GetAxis (verticalAxis);
+
+		rigidBody.velocity = new Vector2 (movex * PlayerSpeed, movey * PlayerSpeed);
+
+		if (movex != 0 || movey != 0) {
+			lastDirection = new Vector2 (movex, movey);
+		}
+
+		Shoot(lastDirection);
+	}
+
+
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
