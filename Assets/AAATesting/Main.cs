06707b53b74bd0f2d38cf3a8729664f4a6ed17c5
zoox101 <william@thebookers.net>
2016-04-17 00:03:39 -0500

    Pulling it all together...

--- Assets/AAATesting/Main.cs
+++ Assets/AAATesting/Main.cs
@@ -4,12 +4,14 @@ using System.Collections;
 public class Main : MonoBehaviour {
 
 	public GameObject player;
-	public GameObject greenememy;
+	public GameObject greenenemy;
 	public GameObject redenemy;
 	public GameObject blueenemy;
+	public UIController ui;
 
 	public int maxenemies;
-	public ArrayList enemies;
+	//Blue, Red, Green
+	public int[] colorcount;
 
 	// Use this for initialization
 	void Start () {
@@ -17,9 +19,25 @@ public class Main : MonoBehaviour {
 		//TODO: Getobjects from gamestate
 		//player = GameState.gameState.getPlayer ();
 		//enemy = GameState.gameState.getEnemy ();
+		//ui = GameState.gameState.getUi();
 
-		enemies = new ArrayList ();
+		colorcount = new int[3];
 		player = Instantiate (player);
+
+		GameObject newenemy;
+		for (int i = 0; i < maxenemies - 2; i+=3) {
+			colorcount [0]++;
+			newenemy = Instantiate (blueenemy);
+			MapController.spawn (newenemy);
+			colorcount [1]++;
+			newenemy = Instantiate (redenemy);
+			MapController.spawn (newenemy);
+			colorcount [2]++;
+			newenemy = Instantiate (greenenemy);
+			MapController.spawn (newenemy);
+		}
+
+		spawnEnemies ();
 	
 	}
 	
@@ -27,31 +45,65 @@ public class Main : MonoBehaviour {
 	void Update () {
 
 		//While there aren't enough enemies, spawn more
-		spawnEnemies ();
 
 
 	}
 		
 	private void spawnEnemies() {
 
-		while (enemies.Count < maxenemies) {
-			GameObject newenemy = Instantiate (redenemy);
-			enemies.Add (newenemy);
+		int totalenemies = colorcount [0] + colorcount [1] + colorcount [2];
+		while (totalenemies < maxenemies) {
+
+			float[] spawnrates = new float[2];
+
+			if (totalenemies != 0) {
+				spawnrates [0] = ((float)colorcount [0]) / totalenemies;
+				spawnrates [1] = ((float)colorcount [1]) / totalenemies + spawnrates [0];
+			} else {
+				spawnrates [0] = 0.33333f;
+				spawnrates [1] = 0.66666f;
+			}
+
+			float random = Random.value;
+			Debug.Log (Random.value);
+
+			GameObject newenemy;
+			if (random < spawnrates [0]) {
+				colorcount[0]++;
+				newenemy = Instantiate (blueenemy);
+			} else if (spawnrates [0] <= random && random <= spawnrates [1]) {
+				colorcount [1]++;
+				newenemy = Instantiate (redenemy);
+			} else {
+				colorcount [2]++;
+				newenemy = Instantiate (greenenemy);
+			}
+
+			totalenemies = colorcount [0] + colorcount [1] + colorcount [2];
 			MapController.spawn (newenemy);
+			checkLose ();
 		}
 	}
 
-	public void spawnPlayer() {
+	private void spawnPlayer() {
 		MapController.spawn (player);
 	}
 
 	public void removeEnemy(GameObject enemy) {
-
+		//TODO: Reimplement these lines
+		int inversecolor = 0;
+		//int inversecolor = enemy.GetComponent<Enemy> ().Shape;
+		colorcount[2 - inversecolor]--;
 		Destroy (enemy);
+		spawnEnemies ();
 	}
 
 	public void removePlayer(GameObject player) {
+		spawnPlayer ();
+	}
 
-		Destroy (player);
+	private void checkLose() {
+		if (colorcount [0] == maxenemies || colorcount [1] == maxenemies || colorcount [2] == maxenemies)
+			Debug.Log ("GameOver");
 	}
 }
