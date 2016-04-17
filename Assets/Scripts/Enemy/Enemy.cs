f1fb61f6c09fa091f33e2e1f852b063aac82210a
Eric Garrison <egarrisn@swbell.net>
2016-04-16 20:27:21 -0500

    Stopped selecting self as target

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -10,13 +10,13 @@ public class Enemy : BaseCharacter {
 
 	void FixedUpdate() {
 		BaseCharacter[] targets = GameObject.FindObjectsOfType<BaseCharacter> ();
-		GameObject[] obstacles = GameObject.FindGameObjectsWithTag("Wall");
+		//GameObject[] obstacles = GameObject.FindGameObjectsWithTag("Wall");
 
 		double max = double.MaxValue;
 		BaseCharacter target = null;
-
+		System.Console.WriteLine ("finding closest enemy");
 		foreach (BaseCharacter bc in targets) {
-			if (!(this.CompareTag (bc.tag))) {
+			if (!(bc.Equals(this))) {
 				double dist = Mathf.Sqrt ((bc.transform.position.x - this.transform.position.x) + (bc.transform.position.y - this.transform.position.y));
 				if (dist < max) {
 					max = dist;
@@ -29,7 +29,7 @@ public class Enemy : BaseCharacter {
 		movey = target.transform.position.y - transform.position.y;
 
 		rigidBody.velocity = new Vector2 (movex, movey);
-		rigidBody.velocity = rigidBody.velocity.normalized * PlayerSpeed;
+		//rigidBody.velocity = rigidBody.velocity.normalized * PlayerSpeed;
 
 		if (movex != 0 || movey != 0) {
 			lastDirection = rigidBody.velocity;
