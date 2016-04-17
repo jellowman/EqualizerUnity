412e926f5ed35c33eb6ae96d63d4e3b99efec7f2
Eric Garrison <egarrisn@swbell.net>
2016-04-16 20:30:17 -0500

    removed debugging output

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -14,7 +14,7 @@ public class Enemy : BaseCharacter {
 
 		double max = double.MaxValue;
 		BaseCharacter target = null;
-		System.Console.WriteLine ("finding closest enemy");
+
 		foreach (BaseCharacter bc in targets) {
 			if (!(bc.Equals(this))) {
 				double dist = Mathf.Sqrt ((bc.transform.position.x - this.transform.position.x) + (bc.transform.position.y - this.transform.position.y));
@@ -29,7 +29,7 @@ public class Enemy : BaseCharacter {
 		movey = target.transform.position.y - transform.position.y;
 
 		rigidBody.velocity = new Vector2 (movex, movey);
-		//rigidBody.velocity = rigidBody.velocity.normalized * PlayerSpeed;
+		rigidBody.velocity = rigidBody.velocity.normalized * PlayerSpeed;
 
 		if (movex != 0 || movey != 0) {
 			lastDirection = rigidBody.velocity;
